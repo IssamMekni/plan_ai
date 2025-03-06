e571f61d0514d3075b72119ba45b046916bc40ca
souhaieb <askri@souhaieb.com>
2025-03-06 11:38:46 +0100

    add more feathers to Home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,19 +1,27 @@
 import ProjectBtn from "@/components/ProjectBtn";
-import exempl from "@/tmp/exemplmyproject.json"
+import exempl from "@/tmp/exemplmyproject.json";
+import exempl2 from "@/tmp/exemplComunityProject.json";
+import MoreButton from "@/components/Morebtn";
 export default function page() {
-
   return (
     <div>
-      <div className="m-2 md:w-2/3 lg:w-1/2 sm:m-auto grid grid-cols-1 flex-col gap-4">
-        <h2 className="text-3xl font-bold">Your Project :</h2>
-        <div>
-
-        {exempl.map((project)=>(
-          <ProjectBtn key={project.name} project={project} />
-        )
-      )}
+      <div className="m-2 md:w-2/3 lg:w-1/2 sm:m-auto grid grid-cols-1 flex-col ">
+        <h2 className="text-3xl font-bold ">Your Project :</h2>
+        <div className="flex flex-col gap-4">
+          {exempl.map((project) => (
+            <ProjectBtn key={project.name} project={project} />
+          ))}
+          <MoreButton link="/"/>
+        </div>
       </div>
-
+      <div className="m-2 md:w-2/3 lg:w-1/2 sm:m-auto grid grid-cols-1 flex-col pt-10 ">
+        <h2 className="text-3xl font-bold">Comunity Project :</h2>
+        <div className="grid grid-cols-2 gap-4">
+          {exempl2.map((project) => (
+            <ProjectBtn key={project.name} project={project} />
+          ))}
+          <MoreButton link="/"/>
+        </div>
       </div>
     </div>
   );
--- src/components/ProjectBtn/index.tsx
+++ src/components/ProjectBtn/index.tsx
@@ -1,7 +1,13 @@
 import { truncateText } from "@/lib/truncateText";
-import { truncateArray } from "@/lib/truncateArray";
-import { Workflow } from "lucide-react";
+import { ArrowRight, Workflow } from "lucide-react";
 import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "../ui/button";
 
 interface Project {
   imageUrl: string;
@@ -19,40 +25,50 @@ interface ProjectBtnProps {
 
 const ProjectBtn: React.FC<ProjectBtnProps> = ({ project }) => {
   return (
-    <Link href={project.link} className="block w-full">
-      <div className="border p-4 rounded-lg shadow-lg flex flex-col gap-4 bg-primary-foreground hover:shadow-xl transition-shadow duration-300">
-        <h3 className="font-semibold text-xl text-gray-900">{project.name}</h3>
-        
-        {project.description && (
-          <p className="text-gray-500 text-sm">{truncateText(project.description, 100)}</p>
-        )}
-        
-        <div className="flex flex-col md:flex-row gap-6">
-          <div className="flex-1">
-            <h3 className="font-bold text-lg text-gray-700">Diagrams:</h3>
-            <ul className="grid gap-2 list-disc pl-4 text-gray-600">
-              {truncateArray(project.diagrams).map((diagram) => (
-                <li key={diagram.name} className="bg-primary/10 px-2 py-1 rounded-md text-gray-500 text-sm">
-                  {/* {truncateText(diagram.name, 10, 20)} */}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="relative w-full md:w-2/5 h-40 rounded-lg overflow-hidden shadow-md">
-            <img
-              src={project.imageUrl}
-              alt={project.name}
-              className="object-cover w-full h-full"
-            />
-            <p className="absolute bottom-2 right-2 bg-primary/70 text-white px-3 py-1 flex items-center gap-2 rounded-lg text-lg font-semibold">
-              {project.diagramsCount} <Workflow size={20} />
-            </p>
+    <div className="border p-4 rounded-lg shadow-lg flex flex-col gap-4 bg-primary-foreground hover:shadow-xl transition-shadow duration-300">
+      <h3 className="font-semibold text-xl text-white">{project.name}</h3>
+
+      {project.description && (
+        <p className="text-gray-500 text-sm">
+          {truncateText(project.description, 100)}
+        </p>
+      )}
+
+      <div className="flex flex-col  gap-6">
+        <div className="flex-1">
+          {/* <h3 className="font-bold text-lg text-gray-700 hidden sm:block">Diagrams:</h3> */}
+        </div>
+        <div className="relative w-full h-40 rounded-lg  shadow-md">
+          <img
+            src={project.imageUrl}
+            alt={project.name}
+            className="object-cover w-full h-full"
+          />
+          <div className="">
+            <DropdownMenu>
+              <DropdownMenuTrigger className="  absolute bottom-2 right-2 bg-primary/70 text-white px-3 py-1 flex items-center gap-2 rounded-lg text-lg font-semibold">
+                {project.diagramsCount} <Workflow size={20} />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="bg-background/60 backdrop-blur-sm">
+                {project.diagrams.map((diagram) => (
+                  <DropdownMenuItem key={diagram.name}>
+                    {diagram.name}
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
-        
-        <p className="text-gray-500 text-sm">📅 Date: {project.date}</p>
       </div>
-    </Link>
+      <div className="flex justify-between">
+        <p className="text-gray-500 text-sm">📅 Date: {project.date}</p>
+        <Link href={project.link} className=" ">
+        <Button className="font-bold text-lg">
+          go <ArrowRight/>
+        </Button>
+        </Link>
+      </div>
+    </div>
   );
 };
 
