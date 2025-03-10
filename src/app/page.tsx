acc36f39fd760bf1761028af0b335270930b9880
souhaieb <askri@souhaieb.com>
2025-03-10 09:25:10 +0100

    prisma with authjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { JSX } from "react";
 import Navbar from "./Navbar";
+import { auth } from "@/auth";
 
-export default function Home():JSX.Element {
+export default async function Home():Promise<JSX.Element> {
+  const session = await auth();
   return (
     <div>
       <Navbar/>
       <br />
+      {JSON.stringify(session)}
       landing page
     </div>
   );
--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,25 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  })],
-  session: {
-    strategy: "jwt",
-    maxAge: 1 * 24 * 60 * 60, // 1 days
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./lib/prisma";
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  // callbacks: {
+  //   async session({ session, user }) {
+  //     session.user.id = user.id; // إضافة معرف المستخدم إلى الجلسة
+  //     return session;
+  //   },
+  // },
+  pages: {
+    signIn: '/auth/signin', // صفحة تسجيل الدخول المخصصة (اختياري)
   },
-})
+};
+
+export const { handlers, auth, signIn, signOut } = NextAuth(authOptions);
